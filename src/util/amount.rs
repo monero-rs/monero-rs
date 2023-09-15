c8b19f23f5b498132c816e5a64a836ef8f0d430c
h4sh3d <h4sh3d@protonmail.com>
2023-09-15 15:25:58 +0200

    chore: update year in headers

--- src/util/amount.rs
+++ src/util/amount.rs
@@ -1,5 +1,5 @@
 // Rust Monero Library
-// Written in 2021-2022 by
+// Written in 2021-2023 by
 //   Monero Rust Contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
